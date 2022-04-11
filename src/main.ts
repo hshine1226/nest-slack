174cf1d372b13d6dd0c6d508840aeef7ce78d732
hshine1226 <hshine1226@gmail.com>
2022-04-11 21:22:13 +0900

    Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
@@ -6,6 +7,16 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || '3000';
+
+  const config = new DocumentBuilder()
+    .setTitle('nest-slack API')
+    .setDescription('The nest-slack API description')
+    .setVersion('1.0')
+    .addTag('slack')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   console.log(`Listening on port ${port}`);
   if (module.hot) {
